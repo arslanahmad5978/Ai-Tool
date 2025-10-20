c30141248bb66bfff3374e2c331a5ef11835475f
Arslan Ahmad <53128996+arslanahmad5978@users.noreply.github.com>
2025-10-20 05:01:00 +0500

    Update ai_tool.py

--- ai_tool.py
+++ ai_tool.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime, timedelta
 
 # YouTube API Key
-API_KEY = "Enter your API Key here"
+API_KEY = "AIzaSyDpg5IspCa_V23iiY0c9w7yI3nB-IYdIDQ"
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
 YOUTUBE_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels"
